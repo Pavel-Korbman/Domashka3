5f6c48602d24c3f15ad00cb4e087f5e86befc576
Pavel <pavel@b2profit.ru>
2023-10-14 01:20:56 +0300

    Решил 3 задачу

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if((number % 2) == 0)
+{
+    Console.WriteLine($"Число `{number}` чётное");
+}
+else
+{
+ Console.WriteLine($"Число `{number}` нечётное");   
+}
